50c786a932f3c9bef7525a7e765ccc174d57583a
Ui-odi <odilia.wang@gmail.com>
2024-08-14 08:10:58 +0800

    update code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const text = ref('這是一段文字')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>{{ text }}</h1>
+    <input type="text" v-model="text" />
+  </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/for-vie-2024-deploy/',
   plugins: [
     vue(),
     vueDevTools(),
