3b049f82b23fe7d59ca6e0a998347ce89dff605e
Ui-odi <odilia.wang@gmail.com>
2024-08-16 12:25:47 +0800

    update code

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/for-vie-2024-deploy/',
+  base: '/for-vue-2024-deploy/',
   plugins: [
     vue(),
     vueDevTools(),
